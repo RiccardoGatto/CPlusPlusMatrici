2516a9c5988bcacd380cfbb3c59eb96c60dc8ff7
RiccardoGatto <gatto.riccardo@gmail.com>
2023-03-08 18:50:48 +0100

    Primi esercizi matrici

---
+++ primiEserciziMatrici.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <random>
+using namespace std;
+const int righe = 9;
+const int colonne = 9;
+
+void riempiMat(int mat[][colonne], int rig, int col)
+{
+    for (int i = 0; i < rig; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            mat[i][j] = rand() % 10;
+        }
+    }
+}
+
+void stampaTrattini(int colonne)
+{
+    cout << "    ";
+    for (int i = 0; i < colonne; i++)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+    int trattini = colonne * 2 + 4;
+    for (int i = 0; i < trattini; i++)
+    {
+        cout << "-";
+    }
+    cout << endl;
+}
+
+void stampaMat(int mat[][colonne], int rig, int col)
+{
+    for (int i = 0; i < rig; i++)
+    {
+        cout << i << "|  ";
+
+        for (int j = 0; j < col; j++)
+        {
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void stampaMagg5(int mat[][colonne], int rig, int col)
+{
+    for (int i = 0; i < rig; i++)
+    {
+        cout << i << "|  ";
+
+        for (int j = 0; j < col; j++)
+        {
+            if (mat[i][j] >= 5)
+            {
+                cout << mat[i][j] << " ";
+            }
+            else
+            {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+int sommaMat(int mat[][colonne], int rig, int col)
+{
+    int somma = 0;
+    for (int i = 0; i < rig; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            somma += mat[i][j];
+        }
+    }
+    return somma;
+}
+
+void diagonaleNove(int mat[][colonne], int rig, int col)
+{
+    for (int i = 0; i < rig; i++)
+    {
+        cout << i << "|  ";
+
+        for (int j = 0; j < col; j++)
+        {
+            if (i==j or i+j==8)
+            {
+                cout << "9 ";
+            }
+            else
+            {
+                cout << "0 ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int matrice[righe][colonne];
+    riempiMat(matrice, righe, colonne);
+    stampaTrattini(colonne);
+    stampaMat(matrice, righe, colonne);
+    cout << "______________________________________________________\n\n";
+    stampaTrattini(colonne);
+    stampaMagg5(matrice, righe, colonne);
+    cout << "______________________________________________________\n\n";
+    cout << "La somma di tutti i numeri della matrice e' " << sommaMat(matrice, righe, colonne) << endl;
+    cout << "______________________________________________________\n\n";
+    stampaTrattini(colonne);
+    diagonaleNove(matrice, righe, colonne);
+}
